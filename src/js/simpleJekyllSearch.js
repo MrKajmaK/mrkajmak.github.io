9eb315b3a4ff8bbf77fe2c6d73765d2a2f574521
kovansky <skowanski@f4dev.me>
2021-05-26 21:28:42 +0200

    Polish Kendama Community added to portfolio

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -7,7 +7,7 @@
             searchResults   : '.search-results',
             searchResultsTitle   : '<h4>Search Results:</h4>',
             limit           : '5',
-            noResults       : '<p>Holy guacamole!<br/><small>Nic nie znaleziono! :(</small></p>'
+            noResults       : '<p>Holy guacamole!<br/><small>Nothing found! :(</small></p>'
         }, options);
 
         var properties = settings.jsonFormat.split(',');
